47709dcf4a63b0743c62f5339c9f952e1bdecf24
jestebanDesarrollo <jestebandesarrollador@gmail.com>
2024-10-15 23:16:22 -0500

    Se agrega logo al menu ademas de botones de redes sociales y descarga de hoja de vida v1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component'
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent,BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Juan E.M.M';
 }
